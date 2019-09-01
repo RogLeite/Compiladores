d3f5216fb9fd0cb3cd8bbab892f43e22e152421b
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-09-01 18:17:59 -0300

    Completa parser e cria outros códigos fonte para serem usados durante os testes

---
+++ src/main/generate_raw.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "interpreter.h"
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+
+char* names[] = {
+          NULL,
+          "TK_AS",
+          "TK_CHAR",
+          "TK_ELSE",
+          "TK_FLOAT",
+          "TK_IF",
+          "TK_INT",
+          "TK_NEW",
+          "TK_RETURN",
+          "TK_VOID",
+          "TK_WHILE",
+          "TK_ID",
+          "TK_STRING",
+          "TK_INTEGER",
+          "TK_FLOATING",
+          "TK_SYMBOL"
+};
+
+int main(void) {
+  int ntoken, vtoken;
+  ntoken = yylex();
+  printf("int[ x ] = {");
+  int tkcounter = 0;
+  while (ntoken) {
+    printf("%s, ", names[ntoken]);
+    ++tkcounter;
+    ntoken = yylex();
+  }
+  printf("}; x = %d;", tkcounter);
+  return 0;
+}
