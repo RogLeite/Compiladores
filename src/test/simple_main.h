c3371a26adc24caaf16af053574a14b249831453
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-09-01 18:16:40 -0300

    cria headers para testes

---
+++ src/test/simple_main.h
@@ -0,0 +1,9 @@
+#ifndef TEST_SIMPLE_MAIN_H
+#define  TEST_SIMPLE_MAIN_H
+
+#include "main/interpreter.h"
+
+char* test_name = "simple_main.h";
+int result[19] = {TK_ID, TK_SYMBOL, TK_SYMBOL, TK_SYMBOL, TK_INT, TK_SYMBOL, TK_ID, TK_SYMBOL, TK_INT, TK_SYMBOL, TK_ID, TK_SYMBOL, TK_INTEGER, TK_SYMBOL, TK_RETURN, TK_ID, TK_SYMBOL, TK_SYMBOL };
+
+#endif
