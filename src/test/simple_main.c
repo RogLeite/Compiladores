1802498f523629924297fdb825d1dd9065a8b50e
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-09-04 13:13:20 -0300

    Mais guaribada ainda

--- src/test/simple_main.c
+++
@@ -1,5 +0,0 @@
-#include "tester.h"
-#include "main/interpreter.h"
-
-char* test_name = "simple_main.h";
-int result[19] = {TK_ID, TK_SYMBOL, TK_SYMBOL, TK_SYMBOL, TK_INT, TK_SYMBOL, TK_ID, TK_SYMBOL, TK_INT, TK_SYMBOL, TK_ID, TK_SYMBOL, TK_INTEGER, TK_SYMBOL, TK_RETURN, TK_ID, TK_SYMBOL, TK_SYMBOL };
