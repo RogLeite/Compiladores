1fa5d05667ebaa57c8624483cb66006b165255e1
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-09-24 12:18:45 -0300

    Começa a definir a árvore sintática

--- src/main/interpreter.h
+++ src/main/interpreter.h
@@ -3,60 +3,50 @@
 
 #include "main/definitions.h"
 
-union node_t;
-typedef union node_t Node;
+typedef union tree_node_t Node;
 
-typedef enum tree_tag_t{
-  Eadd,
-  Esub,
-  Emul,
-  Ediv,
+typedef enum node_tag_t{
+  expression,
+  block,
+  declaracao,
+  comando,
+  tipo,
+  variavel
+} NodeTag;
 
-} TreeTag;
+typedef union tree_node_t{
+  struct{
+    NodeTag tag;
+  } leaf;
+  struct{
+    NodeTag tag ;
+    Node *e1;
+  } uni;
+  struct{
+    NodeTag tag ;
+    Node *e1,*e2;
+  } bi;
+  struct{
+    NodeTag tag ;
+    Node *e1,*e2,*e3;
+  } tri;
+} Node;
 
-
+Node *global_tree;
 /**
 Como está no caderno
 */
-typedef struct exp_t Exp;
-typedef struct exp_t{
-  TreeTag tag;
+//typedef struct exp_t Exp;
+/*
+typedef struct tree_node_t{
+  NodeTag tag;
   union content_t{
     struct{ } leaf;
     struct{ Exp *e1; } uni;
     struct{ Exp *e1,*e2; } bi;
     struct{ Exp *e1,*e2,*e3; } tri;
   } content;
-} Exp;
-
-/**
-Minha tentativa
-*/
-/*
-typedef struct leaf_node_t{
-  TreeTag tag;
-} LeafNode;
-
-typedef struct uni_node_t{
-  TreeTag tag;
-  Node* next;
-} UniNode;
-
-typedef struct bi_node_t{
-  TreeTag tag;
-  Node* e1;
-  Node* e2;
-} BiNode;
-
-typedef union node_t{
-  LeafNode leaf_node;
-  UniNode uni_node;
-  BiNode bi_node;
 } Node;
 */
-
-
-
-
 
 #endif
