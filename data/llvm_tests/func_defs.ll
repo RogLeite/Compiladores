d479a04ab12327993bfdf20d5b1adc50e85453e9
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-11-02 18:58:59 -0300

    define teste llvm `test_t5.c`

--- data/llvm_tests/func_defs.ll
+++
@@ -1,6 +0,0 @@
-define void @foo() #0 {
-  ret void
-}
-define i32 @main() #0 {
-  ret i32 0
-}
---
+++ data/llvm_tests/test_t5.c
@@ -0,0 +1,12 @@
+void foo(void)
+{
+  printf("%d", 3);
+  printf("\n");
+}
+int main(void)
+{
+  printf("%d", 1);
+  printf("\n");
+  printf("%d", 2);
+  printf("\n");
+}
