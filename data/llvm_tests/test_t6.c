2c1a32a517097550fbdeb9220a77e31d0c907051
RogLeite <rodrigo.ldasilva@hotmail.com>
2019-11-03 11:33:51 -0300

    Correspondente em c de `test_t6.mng`

---
+++ data/llvm_tests/test_t6.c
@@ -0,0 +1,14 @@
+int var1;
+int var2;
+int main(void)
+{
+  var1 = 1;
+  var2 = 2;
+  printf("%d", var1);
+  printf("\n");
+  printf("%d", var2);
+  printf("\n");
+  printf("%d", 3);
+  printf("\n");
+  return 0;
+}
